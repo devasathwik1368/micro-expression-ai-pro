c9103a7e354cd063780e07c0f2e7145ce57f7a95
devasathwik1368 <161280813+devasathwik1368@users.noreply.github.com>
2026-03-25 14:03:07 +0530

    Update 1_Visual_Analytics.py

--- pages/1_Visual_Analytics.py
+++ pages/1_Visual_Analytics.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("📊 Behavioral Analytics Dashboard")
+st.title("📊 Behavioral Analytics")
 
 if not st.session_state.data:
     st.warning("No data found. Capture expressions on the Main Page first.")
@@ -10,10 +10,10 @@ else:
     df = pd.DataFrame(st.session_state.data)
     c1, c2 = st.columns(2)
     with c1:
-        st.write("### Emotion Timeline")
-        st.line_chart(df.set_index("Timestamp")["Intensity"])
+        st.write("### Emotion Intensity Timeline")
+        st.line_chart(df.set_index("Timestamp")["Confidence"])
     with c2:
-        st.write("### Distribution")
-        fig, ax = plt.subplots(facecolor='#0E1117')
-        df['Emotion'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax, textprops={'color':"w"})
+        st.write("### Emotion Distribution")
+        fig, ax = plt.subplots()
+        df['Emotion'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax)
         st.pyplot(fig)
